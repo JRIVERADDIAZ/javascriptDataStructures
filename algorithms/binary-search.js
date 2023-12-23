5f8fc52bcf6e6199d2fe506af7741848a24aae9c
JRIVERADDIAZ <jovahinrivera@gmail.com>
2023-12-23 16:43:48 +0000

    Arrays-update

--- algorithms/binary-search.js
+++ algorithms/binary-search.js
@@ -4,18 +4,26 @@
  *  match the value were loking for
 *Match Found:
 
-* If the middle element is equal to the target element, you've found a match. In this case, you can return the index of the middle element as the result, indicating that the target has been found.
+* If the middle element is equal to the target element, you've found a match. In this case, you can return the index of the middle
+*  element as the result, indicating that the target has been found.
 * Target Element is Greater:
 
-* If the middle element is greater than the target element, this means the target element must be in the left half of the remaining search range, as the list is sorted in ascending order.
-* Update the "high" pointer to be one less than the middle index. This effectively reduces the search range to the left half of the list.
-* Continue the search within this reduced range.
+* If the middle element is greater than the target element, this means the target element must be in the left half of the
+* remaining search range, as the list is sorted in ascending order.
+* Update the "high" pointer to be one less than the middle index. This effectively reduces the search range
+* to the left half of the list. Continue the search within this reduced range.
 * Target Element is Smaller:
 
 * If the middle element is smaller than the target element, this indicates that the target element is in the right half of the remaining search range.
 * Update the "low" pointer to be one more than the middle index, narrowing the search range to the right half of the list.
+*
 * Continue the search within this reduced range.
-*/
+*
+* para que se usa
+* como se usa
+*
+*
+* */
 
 function binarySearch(arr, target) {
     let left = 0;
--- data-structures/Arrays.js
+++ data-structures/Arrays.js
@@ -1,45 +1,108 @@
-let arr = [1, 2, 3, 4, 5];
-const showData = (arr) => {
+//creatig new arrays 
 
-    if (arr.length === 0) {
-        console.log('Array is empty');
-        return;
+// let allDaysOfTheWeek = new Array();
+// let allDaysOfTheWeek = new Array(7);
+// let allDaysOfTheWeek = Object.freeze(new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday',
+//    'Thursday', 'Friday', 'Saturday'));
+
+
+/*
+   let daysOfTheWeek = (day) => {
+    for (let i = 0; i < allDaysOfTheWeek.length; i++) {
+        if (allDaysOfTheWeek[i] === day) console.log('exist');
     }
+}
+*/
 
-    let lastElement = arr.pop();
-    console.log(lastElement);
+// daysOfTheWeek('Monday')
 
-    showData(arr);
+// fibbonacci series using Arrays DS
+
+/* 
+let fibo = [];
+fibo[1] = 1;
+fibo[2] = 2;
+
+for(var i = 3; i < 20; i++) {
+    fibo[i] = fibo[i-1] + fibo[i-2];
 }
 
-const addData = (arr, item) => {
-    const arrEnd = arr.length;
-    arr[arrEnd] = item;
-    return arr;
+for(var j = 1; j < fibo.length; j++) {
+    console.log(fibo[j]);
+}   
+*/
+
+/*
+let numbers = new Array();
+
+const addNUmber = (element) => {
+    numbers.unshift(element);
+    console.log('beggining added', numbers);
 }
 
-const removeData = (arr) => {
-// remove from the top
+const removeNumber = (element) => {
+    numbers.splice(element, 1);
+    console.log('removed', numbers);
+}
 
-    if (arr.length === 0) {
-        console.log('Array is empty');
-        return;
+const searchNumber = (element) => {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] === element) {
+            console.log('found')
+            return true
+        };
     }
+    console.log('not found');
+    return false;
+}
 
-    arr.shift();
-    return arr;
-};
+const removeAt = (index) => {
+    numbers.splice(index, 1)
+    console.log('removed', numbers);
+}
 
-const insertInSpecificArrayIndex = (arr, item, index) => {
+const insertAt = (element, index) => {
+    numbers.splice(index, 0, element)
+    console.log('inserted', numbers);
+}   
+ */
 
-    for (let i = arr.length; i >= index; i--) {
-        arr[i] = arr[i - 1];
-    }
+
+// Multiple pointers pattern
+
+var averageTempDay1 = new Array(72, 75, 79, 79, 81, 81);
+var averageTempDay2 = new Array(81, 79, 75, 75, 73, 72);
+
+let averageTemp = new Array(averageTempDay1, averageTempDay2);
+
+// //day 1
+// console.log(averageTemp[0], 'averageTempDay1');
+// console.log(averageTemp[0][0]);
+// console.log(averageTemp[0][1]);
+// console.log(averageTemp[0][2]);
+// console.log(averageTemp[0][3]);
+// console.log(averageTemp[0][4]);
+// console.log(averageTemp[0][5]);
+// //day 2
+// console.log(averageTemp[1], 'averageTempDay2');
+// console.log(averageTemp[1][0]);
+// console.log(averageTemp[1][1]);
+// console.log(averageTemp[1][2]);
+// console.log(averageTemp[1][3]);
+// console.log(averageTemp[1][4]);
+// console.log(averageTemp[1][5]);
+
+// Traverse multidinmensional array
+
+// const traverseMatrix = (myMatrix) => {
+//     for (let i = 0; i < myMatrix.length; i++) {
+//         for (let j = 0; j < myMatrix[i].length; j++) {
+//             console.log(myMatrix[i][j]);
+//         }
+//     }
+// }
+
+traverseMatrix(averageTemp);    
 
 
 
-};
-
-// showData(arr);
-// console.log(addData(arr, 1));
-// console.log(removeData(arr));
